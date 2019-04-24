3720c9e99e6a74c2285ed9f26735b24af08d9e85
hoangnn <hoangnn@sdtech.vn>
2019-04-24 17:13:48 +0700

    file server

---
+++ server.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+const http = require('http');
+
+const app = require('./app');
+
+const port = process.env.PORT || 6969;
+
+const server = http.createServer(app);
+// const app = express();
+
+server.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
+// app.listen(port, () => {
+//     console.log(`server is running on port ${port}`);
+// })
